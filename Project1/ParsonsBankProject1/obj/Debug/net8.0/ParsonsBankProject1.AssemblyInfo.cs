4b0fb2296bbbc846a1cb709661e45ac41e1fe5ed
Parsonsda <david.a.parsons3@gmail.com>
2024-05-06 16:02:18 -0400

    Updates

--- Project1/ParsonsBankProject1/obj/Debug/net8.0/ParsonsBankProject1.AssemblyInfo.cs
+++ Project1/ParsonsBankProject1/obj/Debug/net8.0/ParsonsBankProject1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ParsonsBankProject1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f32fd20628bf35b7540890ff581bd2f7c5561a7f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a621ba7584708e26e18cf6c0464800b9010f5ddd")]
 [assembly: System.Reflection.AssemblyProductAttribute("ParsonsBankProject1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ParsonsBankProject1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
